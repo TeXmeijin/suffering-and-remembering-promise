cd5e354951e023394dabc5bc8c7e9f3e820c114e
TeXmeijin <i8858garden@gmail.com>
2022-03-03 19:05:45 +0900

    comment

--- src/09-ResolveOrRejectFunc/resolveOrReject.ts
+++ src/09-ResolveOrRejectFunc/resolveOrReject.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 (async () => {
-  const id = Math.random() > 0.5 ? 1 : 'NotFound';
-  console.log({ id });
+  // 現実世界のAPIを模倣するために、半々の確率で失敗するように実装
+  const validOrInvalidId = Math.random() > 0.5 ? 1 : 'NotFound';
+  console.log({ id: validOrInvalidId });
   const response = await axios
-    .get<{ name: string }>('https://swapi.dev/api/people/' + id)
+    .get<{ name: string }>('https://swapi.dev/api/people/' + validOrInvalidId)
     .catch((_) => undefined);
 
   if (!response) {
